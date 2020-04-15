40cc7394ca1711b32daa72fc67a5b88ac544864b
N0806708 <samuel.sutton2018@my.ntu.ac.uk>
2020-04-15 20:30:17 +0100

    Finished Basic Menu Layout

---
+++ 01 Blank Game/Blank Game/Menu1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Blank_Game
+{
+    public class Menu1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private Color _backgroundColour = Color.CornflowerBlue;
+
+        private List<Component> _gameComponents;
+
+        public Menu1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+            base.Initialize();
+        }
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            var levelPlayButton = new Button(Content.Load<Texture2D>("Controls/Button"), Content.Load<SpriteFont>("Fonts/Font"))
+            {
+                Position = new Vector2(350, 200),
+                Text = "Level Play",
+            };
+
+            levelPlayButton.Click += LevelPlayButton_Click;
+
+            _gameComponents = new List<Component>()
+            {
+                levelPlayButton,
+
+            };
+
+            base.LoadContent();
+        }
+        private void LevelPlayButton_Click(object sender, System.EventArgs e)
+        {
+
+            throw new System.NotImplementedException("Here is where it will link to the Level Play Menu");
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            foreach (var component in _gameComponents)
+                component.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw (GameTime gameTime)
+        {
+            GraphicsDevice.Clear(_backgroundColour);
+            spriteBatch.Begin();
+            foreach (var component in _gameComponents)
+                component.Draw(gameTime, spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+
+
+        }
+    }
+}
