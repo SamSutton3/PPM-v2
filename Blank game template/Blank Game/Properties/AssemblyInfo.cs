4730f67ffb029f892fb7d15fa01605922c405d5c
JLarkin99 <jlarkin2109@gmail.com>
2020-05-02 19:20:53 +0100

    Game name change

--- Blank game template/Blank Game/Properties/AssemblyInfo.cs
+++ Blank game template/Blank Game/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blank Game")]
-[assembly: AssemblyProduct("Blank Game")]
+[assembly: AssemblyTitle("Aim Trainer")]
+[assembly: AssemblyProduct("Aim Trainer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Nottingham Trent University")]
